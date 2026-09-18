d6537739e54d0915a0d27c376b43eabfb6e7a8cd
Giancsa <gian.69889868@gmail.com>
2026-09-18 20:00:37 -0300

    2026.09.18

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
 # importa as dependências
-from flask import Flask 
+from flask import Flask, render_template 
 
 #Inicializar variáveis e componentes 
+
+#Nome do aplicativo (site web) > global
+sitename = "Meu flask"
 
 #Incializa o aplicativo Flask
 app = Flask(__name__) 
  
  # Rota da pagina inicial (rota raiz ou root)
 @app.route("/") 
-def hello_world(): 
-    return "<p>Hello, World!</p>"
+def index(): 
+    return render_template(
+        "home.html",
+        sitename=sitename,
+        
+        )
 
 #Rota simples
-@app.route("/about")
+@app.route("/sobre")
 def about():
-    output= "Sobre nós..."
-    return output 
+    return render_template(
+        "about.html",
+        sitename=sitename
+        )
 
  
 #Ativa o modo DEBUG e o main loop
